d015e59d940456714bbaa1b584a006dfe5ff2fb3
Aiolos <47620043+iamjohnpalencia@users.noreply.github.com>
2020-09-13 19:40:26 +0800

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,124 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.OpenToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.DeleteToolStripMenuItem1 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MarkAsReadToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ReloadToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.AllowUserToAddRows = False
+        Me.DataGridView1.AllowUserToDeleteRows = False
+        Me.DataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill
+        Me.DataGridView1.BackgroundColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.DataGridView1.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.[Single]
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing
+        Me.DataGridView1.GridColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.DataGridView1.Location = New System.Drawing.Point(13, 27)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.ReadOnly = True
+        Me.DataGridView1.RowHeadersVisible = False
+        Me.DataGridView1.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.AutoSizeToAllHeaders
+        Me.DataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect
+        Me.DataGridView1.Size = New System.Drawing.Size(226, 526)
+        Me.DataGridView1.TabIndex = 0
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.OpenToolStripMenuItem, Me.MarkAsReadToolStripMenuItem, Me.ReloadToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(727, 24)
+        Me.MenuStrip1.TabIndex = 2
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'OpenToolStripMenuItem
+        '
+        Me.OpenToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.DeleteToolStripMenuItem1})
+        Me.OpenToolStripMenuItem.Name = "OpenToolStripMenuItem"
+        Me.OpenToolStripMenuItem.Size = New System.Drawing.Size(56, 20)
+        Me.OpenToolStripMenuItem.Text = "Option"
+        '
+        'DeleteToolStripMenuItem1
+        '
+        Me.DeleteToolStripMenuItem1.Name = "DeleteToolStripMenuItem1"
+        Me.DeleteToolStripMenuItem1.Size = New System.Drawing.Size(107, 22)
+        Me.DeleteToolStripMenuItem1.Text = "Delete"
+        '
+        'MarkAsReadToolStripMenuItem
+        '
+        Me.MarkAsReadToolStripMenuItem.Name = "MarkAsReadToolStripMenuItem"
+        Me.MarkAsReadToolStripMenuItem.Size = New System.Drawing.Size(91, 20)
+        Me.MarkAsReadToolStripMenuItem.Text = "Mark As Read"
+        '
+        'ReloadToolStripMenuItem
+        '
+        Me.ReloadToolStripMenuItem.Name = "ReloadToolStripMenuItem"
+        Me.ReloadToolStripMenuItem.Size = New System.Drawing.Size(55, 20)
+        Me.ReloadToolStripMenuItem.Text = "Reload"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.TextBox1.Location = New System.Drawing.Point(245, 27)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(470, 526)
+        Me.TextBox1.TabIndex = 3
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.ClientSize = New System.Drawing.Size(727, 565)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents OpenToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents MarkAsReadToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents DeleteToolStripMenuItem1 As ToolStripMenuItem
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents ReloadToolStripMenuItem As ToolStripMenuItem
+End Class
---
+++ Form1.vb
@@ -0,0 +1,108 @@
+﻿Imports MySql.Data.MySqlClient
+Imports System.IO
+Public Class Form1
+    Public cs As String = "server = localhost" &
+                         ";username=" & "root" &
+                         ";password=" & "" &
+                         ";database=" & "dbmes"
+
+    Public mysqlcon As MySqlConnection = Nothing
+    Public command As MySqlCommand
+
+
+    Private Sub loaddatagrid()
+
+        DataGridView1.Columns.Clear()
+
+        Dim mysqlcon As New MySqlConnection(cs)
+        Dim dt As New DataTable
+        Dim sda As New MySqlDataAdapter
+        Dim bsource As New BindingSource
+        Dim command As New MySqlCommand
+        Try
+
+            mysqlcon.Open()
+            command = New MySqlCommand("SELECT * from tbmessage", mysqlcon)
+
+            DataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill
+            DataGridView1.AutoGenerateColumns = False
+
+            Dim index As Integer
+            index = DataGridView1.Columns.Add("id", "id")
+            DataGridView1.Columns(0).AutoSizeMode = DataGridViewAutoSizeColumnMode.DisplayedCells
+            DataGridView1.Columns(0).Visible = False
+            DataGridView1.Columns(index).DataPropertyName = "id"
+
+
+            index = DataGridView1.Columns.Add("date_", "date_")
+            DataGridView1.Columns(1).Visible = True
+            DataGridView1.Columns(index).DataPropertyName = "date_"
+
+            index = DataGridView1.Columns.Add("message_", "message_")
+            DataGridView1.Columns(2).Visible = True
+            DataGridView1.Columns(index).DataPropertyName = "message_"
+
+            index = DataGridView1.Columns.Add("from_", "from_")
+            DataGridView1.Columns(3).Visible = False
+            DataGridView1.Columns(index).DataPropertyName = "from_"
+
+            index = DataGridView1.Columns.Add("status_", "status_")
+            DataGridView1.Columns(4).Visible = False
+            DataGridView1.Columns(index).DataPropertyName = "status_"
+
+            sda.SelectCommand = command
+            sda.Fill(dt)
+            bsource.DataSource = dt
+            DataGridView1.DataSource = bsource
+            sda.Update(dt)
+
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        loaddatagrid()
+        colorunread()
+    End Sub
+
+
+
+    Private Sub DataGridView1_CellClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView1.CellClick
+        TextBox1.Text = "Message From: " & Me.DataGridView1.Item(3, Me.DataGridView1.CurrentRow.Index).Value.ToString & vbCrLf & "Date: " & Me.DataGridView1.Item(1, Me.DataGridView1.CurrentRow.Index).Value.ToString & vbCrLf & "Content: " & vbCrLf & vbCrLf & Me.DataGridView1.Item(2, Me.DataGridView1.CurrentRow.Index).Value.ToString
+        Me.DataGridView1.Item(4, Me.DataGridView1.CurrentRow.Index).Value = "Read"
+        'updatestat()
+
+        colorunread()
+    End Sub
+
+    Private Sub updatestat()
+
+        mysqlcon = New MySqlConnection
+        mysqlcon.ConnectionString = cs
+        mysqlcon.Open()
+
+        Dim command As New MySqlCommand
+        command.Connection = mysqlcon
+
+        command.CommandText = "UPDATE dbmes.tbmessage set status_ = 'Read' where Id = '" & Me.DataGridView1.Item(0, Me.DataGridView1.CurrentRow.Index).Value.ToString & "'"
+
+        command.ExecuteReader()
+        mysqlcon.Close()
+    End Sub
+    Private Sub colorunread()
+        For i = 0 To DataGridView1.Rows.Count - 1
+            If DataGridView1.Rows(i).Cells(4).Value.ToString = "Unread" Then
+                DataGridView1.Rows(i).DefaultCellStyle.BackColor = Color.LightSkyBlue
+            Else
+                DataGridView1.Rows(i).DefaultCellStyle.BackColor = Color.White
+            End If
+        Next
+    End Sub
+
+    Private Sub ReloadToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ReloadToolStripMenuItem.Click
+        loaddatagrid()
+        colorunread()
+    End Sub
+End Class
